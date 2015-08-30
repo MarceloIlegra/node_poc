01711a336732d07614c498ebf058b46916364d13
MarceloIlegra <marcelo.serpa@ilegra.com>
2015-08-30 14:03:56 -0300

    added method http

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var url = require('url');
 
 http.createServer(function (req, res) {
 	console.log('Url : ' + req.url)
-	if(req.url === '/home/contact'){
+	console.log(req);
+	if(req.url === '/home/contact' && req.method === 'GET'){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
   		res.end('Home Contato \n');
-	} else if(req.url === '/products'){
+	} else if(req.url === '/products' && req.method === 'GET'){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
   		res.end('Product list \n');
+	} else if (req.url === '/products'&& req.method === 'POST'){
+		req.on('data', function(chunk){
+			console.log(chunk.toString());
+		});
+		res.writeHead(201, {'Content-Type': 'text/plain'});
+		res.end('Create Product \n');
 	} else {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
   		res.end('Page ' + req.url + 'not exists \n');
