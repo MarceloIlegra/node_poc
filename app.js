cd31b5580bf64e58595f8b30cb864379c1922974
MarceloIlegra <marcelo.serpa@ilegr.com>
2015-08-30 13:04:57 -0300

    test routes with pure nodejs

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var http = require('http');
+var url = require('url');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+	console.log('Url : ' + req.url)
+	if(req.url === '/home/contact'){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+  		res.end('Home Contato \n');
+	} else if(req.url === '/products'){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+  		res.end('Product list \n');
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+  		res.end('Page ' + req.url + 'not exists \n');
+	}
+
 }).listen(1337, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:1337/');
